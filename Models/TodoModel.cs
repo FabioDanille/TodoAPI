c361e2edae42f8c736d2fa914037e90c1d2f5617
Fabio <fabiodanille@hotmail.com>
2024-04-23 20:14:53 -0300

    Criando Todo Model

---
+++ Models/TodoModel.cs
@@ -0,0 +1,11 @@
+namespace TodoApi.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public bool Done { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DateUpdate { get; set; } = DateTime.Now.ToLocalTime();
+    }
+}
